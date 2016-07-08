3055c1c23e24a6a28279eee89b1108c0fe9b8b64
Nagendra Prasad <wowmylibrary@gmail.com>
2016-07-08 14:28:07 +0530

    port details updated

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var express=require('express');
 var app=express();
 app.use(express.static(__dirname+"/public"));	
 //app.listen(process.env.PORT || 3000);
-app.listen(process.env.PORT || 3000, function () {
+/*app.listen(process.env.PORT || 3000, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+});*/
+
+app.port = process.env.PORT || 8000;
+app.listen(8000);
 
